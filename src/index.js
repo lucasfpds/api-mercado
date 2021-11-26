442cb2736da1d9295df4338ef86bdb26347449f9
Lucas Fernandes <lucasfpd.santos@gmail.com>
2021-11-26 17:25:46 -0300

    Habilitação de cors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 const app = require('./servidor');
-
-
+const cors = require('cors');
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+    })
+)
+console.log('cors abilitado')
 app.listen(process.env.PORT || 3000);
